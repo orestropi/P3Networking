cf3485befa7e3f956df18c3a1c690adf0ccc51e3
orestropi <oropi@wpi.edu>
2021-12-07 03:01:02 -0500

    rvtr

--- clientServer.cpp
+++ clientServer.cpp
@@ -31,33 +31,36 @@ int main(int argc, char *argv[]) {
     //if statement to cal client or server function
     int sock = create_cs3516_socket();
     char buffer[40];
-    struct ip_hdr *bob = (struct ip_hdr *)buffer;
-    struct upd_hdr *sally = (struct upd_hdr *)(buffer+20);
+    struct ip *bob = (struct ip *)buffer;
+    struct udphdr *sally = (struct udphdr *)(buffer+20);
     char* data = (buffer + 28);
 
     printf("Hi I am this socket, %d", sock);	
     
-cs3516_send(sock, buffer, 40, next_hop);
+    //5 is next_hop
+    cs3516_send(sock, buffer, 40, 5);
 
     //router
     while(1){
     char dest_buffer[3000];
 
     int actual = cs3516_recv(sock, dest_buffer, 3000);
-    struct ip_hdr *bob = (struct ip_hdr *)dest_buffer;
-    struct upd_hdr *sally = (struct upd_hdr *)(dest_buffer+20);
+    struct ip *bob = (struct ip *)dest_buffer;
+    struct udphdr *sally = (struct udphdr *)(dest_buffer+20);
     char* data = (dest_buffer + 28);
 
     //dont change addresses in Bob
-
-    bob->ttl--:
-    if (bob->ttl < 1){continue;}
+    bob->ip_ttl--;
+    if (bob->ip_ttl < 1){continue;}
     //where 5 is a real address
-    if(bob->ip_dst == 5){
+    in_addr myaddress = {.s_addr = inet_addr("10.0.0.1")};
+    if(bob->ip_dst.s_addr == myaddress.s_addr){
         // then send to host X
-        cs3516_send(sock, dest_buffer, 40, 5);
+        cs3516_send(sock, dest_buffer, 40, (unsigned long)inet_ntoa(bob->ip_dst));//[5's real IP]);
     }
-        else{}
+        else{
+            //send to host y
+        }
 }
 	close(sock);
 return 0;
